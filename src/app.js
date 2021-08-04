ea50789613239dbb13ae09fbd5b3fdc30d76ee3d
Aline <alineaparecidacintrasilva@gmail.com>
2021-08-03 21:49:25 -0300

    primeiro commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const morgan = require('morgan');
+const config = require('./config');
+const expressLoad = require('express-load');
+
+app.use(morgan(':method | :status | HTTP :http-version | Resposta: :response-time ms | Data: :date[web] | URL: :url'));
+app.use(express.json());
+
+expressLoad('./src/controllers')
+    .into(app);
+
+app.listen(config.portApi, () => {
+    console.log(`[Magalu] - Ativo :D | ${config.urlApi}:${config.portApi}`);
+});
+
+module.exports = app;
