a3e28720872ac1122c840c3ad478ad0d8a5038ef
Aline <alineaparecidacintrasilva@gmail.com>
2021-08-08 13:35:18 -0300

    criação do README.md e arquivo do banco de dados

---
+++ README.md
@@ -0,0 +1,30 @@
+# Desafio Magalu
+
+## Por que Node?
+É a ferramenta que mais tenho familiaridade.
+
+## Requisitos ##
+Node https://nodejs.org/en/  
+Yarn (Opcional) https://classic.yarnpkg.com/en/docs/install#windows-stable  
+MySql https://dev.mysql.com/downloads/
+
+## Instalação ##
+Após clonar o repositório, utilize o comando via terminal  
+`npm install` ou `yarn` para baixar os pacotes de dependências. 
+
+
+## API ##
+Utilizando o comando `yarn dev` a aplicação rodará no seguinte endereço:
+`http://localhost:4500/`
+
+## Autenticação JWT
+No header do postman utilizar:
+
+key: `token`
+
+value: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.Ir22pFtxtJf_a3uZGhAKDTCosLv4nRdOoVHZhx-zZ5U`
+
+## Banco de dados
+O banco encontra-se no arquivo `db-magalu`. É necessário importar o script.
+
+
---
+++ db-magalu.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: magalu-db
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.14-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `scheduling`
+--
+
+DROP TABLE IF EXISTS `scheduling`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `scheduling` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `send_date` datetime DEFAULT NULL,
+  `receiver` varchar(60) DEFAULT NULL,
+  `message` varchar(250) DEFAULT NULL,
+  `message_type` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `scheduling`
+--
+
+LOCK TABLES `scheduling` WRITE;
+/*!40000 ALTER TABLE `scheduling` DISABLE KEYS */;
+INSERT INTO `scheduling` VALUES (7,'2021-09-01 10:00:00','Maria da Silva','Domingo Ouro!',NULL);
+/*!40000 ALTER TABLE `scheduling` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'magalu-db'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-08-08 13:11:19
